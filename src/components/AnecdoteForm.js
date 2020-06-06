aef3177a5661335699e80d1d21ef694ac29a7839
Richard Perrotta <perrottarichard@gmail.com>
2020-06-06 09:39:29 +0700

    6.14 complete

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { createMessage, reset } from '../reducers/notificationReducer'
 import { useDispatch } from 'react-redux'
+import anecdoteService from '../services/anecdoteService'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
-    const addNew = (event) => {
+    const addNew = async (event) => {
         event.preventDefault()
         const content = event.target.saying.value
         event.target.saying.value = ''
-        dispatch(addAnecdote(content))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(addAnecdote(newAnecdote))
         dispatch(createMessage())
         setTimeout(() => {
             dispatch(reset())
